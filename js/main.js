1c78a39e95000232bba5a8a70c2347f583b6d97d
Kellsey Neagley <kellseyneagley@gmail.com>
2019-11-18 07:22:08 -0600

    added pics

--- js/main.js
+++ js/main.js
@@ -2,25 +2,28 @@
 
 $(document).ready(function() {
 
-    $('.carousel').carousel()
+    $('.carousel').carousel();
+
+
+
 
 });
 
 
-var directoryDiv = document.getElementById("directory");
-var innerBoxes = directoryDiv.getElementsByClassName("inner-box");
-for (var i = 0; i < innerBoxes.length; i++) {
-    innerBoxes[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("hov-clicked");
-        if(current != null && current[0] != null && this.classList.contains("hov-clicked")) {
-            current[0].className = current[0].className.replace(" hov-clicked", "");
-        }
-        else if (current != null && current[0] != null) {
-            current[0].className = current[0].className.replace(" hov-clicked", "");
-            this.className += " hov-clicked";
-        }
-        else {
-            this.className += " hov-clicked"
-        }
-    });
-}
+// var directoryDiv = document.getElementById("directory");
+// var innerBoxes = directoryDiv.getElementsByClassName("inner-box");
+// for (var i = 0; i < innerBoxes.length; i++) {
+//     innerBoxes[i].addEventListener("click", function() {
+//         var current = document.getElementsByClassName("hov-clicked");
+//         if(current != null && current[0] != null && this.classList.contains("hov-clicked")) {
+//             current[0].className = current[0].className.replace(" hov-clicked", "");
+//         }
+//         else if (current != null && current[0] != null) {
+//             current[0].className = current[0].className.replace(" hov-clicked", "");
+//             this.className += " hov-clicked";
+//         }
+//         else {
+//             this.className += " hov-clicked"
+//         }
+//     });
+// }
